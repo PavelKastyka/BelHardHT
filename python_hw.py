1cf551d0dc2c5df3c40598a709f42019a2fbdbe3
PavelKastyka <117450125+PavelKastyka@users.noreply.github.com>
2024-10-27 21:04:14 +0100

    Add python script: Print name (#2)

---
+++ python_hw.py
@@ -0,0 +1,16 @@
+first_name = 'Pavel'
+last_name = 'Kastyka'
+age = 16
+print(f'hi, I am {first_name} {last_name} and I am {age} years old')
+print('=================')
+a = 'belhard'
+print(a[2])
+print('=================')
+b = 'BELHARDHT'
+print(b[-2])
+print('=================')
+c = 'BelHardHT'
+print(c[:5])
+print('=================')
+d = 'Python'
+print(d[:-2])
